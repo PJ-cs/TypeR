8e81a0419f273cbb03769bba8322c23d991170bc
Peter Johanns <johannspeter@web.de>
2023-07-03 21:34:21 +0200

    added some files and folders for training, added folder structure for data for SQ1000

--- Image2Letter/config/config.py
+++ Image2Letter/config/config.py
@@ -1,1 +1,14 @@
+# config.py
+from pathlib import Path
+
+# Directories
+BASE_DIR = Path(__file__).parent.parent.absolute()
+CONFIG_DIR = Path(BASE_DIR, "config")
+DATA_DIR = Path(BASE_DIR, "data")
+TYPEWRITER_DIR = Path(DATA_DIR, "SamsungSQ-1000")
+
+# Create dirs
+DATA_DIR.mkdir(parents=True, exist_ok=True)
+TYPEWRITER_DIR.mkdir(parents=True, exist_ok=True)
+
 BLOB_STORE_URL = "gdrive://1xfnkAm5QsUc9l2H4pWZbaOO4P7DbGLv1"
