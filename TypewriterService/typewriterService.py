fixed x limit problem, added test for precision, calib incorrect

TypewriterService/typewriterService.py
@@ -21,7 +21,6 @@ LETTER_LIST: list[str] = [
 ]
 
 arduino: serial.Serial = find_arduino(SERIAL_NUMBER)
-print(arduino.readline())
 def write_letter(x: int, y: int, letter: int, thickness: int) -> str:
     command : str = f"<X{x} Y{y} L{letter} T{thickness}>"
     arduino.write(bytes(command, 'utf-8'))
@@ -41,4 +40,9 @@ def write_img(img: np.ndarray):
                 raise Exception(f"Encountered unexpected arduino response: '{reponse_code}'")
 
 # ham levels start with 1
-print(write_letter(10, 10, 50, 1))
+print(arduino.readline())
+
+for x in range(0, 40):
+    for y in range(0, 40):
+        if write_letter(x, y, 0, 3) != bytes('A', "UTF-8"):
+            exit(1)