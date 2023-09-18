846d55b46cdd83f85e4e243c70fb67ba22ae7944
Peter Johanns <johannspeter@web.de>
2023-09-18 16:05:02 +0200

    fixed img logging again

--- Image2Letter/image2letterAI/models.py
+++ Image2Letter/image2letterAI/models.py
@@ -85,9 +85,9 @@ class TypeRNet(pl.LightningModule):
         loss = self.loss.forward(key_strokes, out_img, img_target)
         self.val_loss_list.append(loss)
         self.log("val_loss", float(loss))
-        if batch_idx % 6 == 0:      
+        if batch_idx % 10 == 0:      
             grid_in = torchvision.utils.make_grid(convert_rgb_tensor_for_plot(img_in[:4])).permute(1,2,0).cpu().numpy()
-            grid_out = torchvision.utils.make_grid(convert_gray_tensor_for_plot(out_img[:4])).permute(1,2,0).cpu().numpy()
+            grid_out = torchvision.utils.make_grid(convert_gray_tensor_for_plot(out_img[:4]), normalize=True).permute(1,2,0).cpu().numpy()
             mlflow.log_image(grid_in, f'validation_rgb_{self.current_epoch}_{batch_idx}.png')
             mlflow.log_image(grid_out, f'validation_out_{self.current_epoch}_{batch_idx}.png')
 
@@ -103,9 +103,9 @@ class TypeRNet(pl.LightningModule):
         out_img, key_strokes = self.forward(img_in)
         loss = self.loss.forward(key_strokes, out_img, img_target)
         self.log("test_loss", float(loss))
-        if batch_idx % 6 == 0:
+        if batch_idx % 10 == 0:
             grid_in = torchvision.utils.make_grid(convert_rgb_tensor_for_plot(img_in[:4])).permute(1,2,0).cpu().numpy()
-            grid_out = torchvision.utils.make_grid(convert_gray_tensor_for_plot(out_img[:4])).permute(1,2,0).cpu().numpy()
+            grid_out = torchvision.utils.make_grid(convert_gray_tensor_for_plot(out_img[:4]), normalize=True).permute(1,2,0).cpu().numpy()
             mlflow.log_image(grid_in, f'test_rgb_{self.current_epoch}_{batch_idx}.png')
             mlflow.log_image(grid_out, f'test_out_{self.current_epoch}_{batch_idx}.png')
         return loss
