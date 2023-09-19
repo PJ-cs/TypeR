fde781a496b128684c77af714a705b52b0117631
Peter Johanns <johannspeter@web.de>
2023-09-19 16:03:34 +0200

    for visulitian remove inversion of output

--- Image2Letter/image2letterAI/models.py
+++ Image2Letter/image2letterAI/models.py
@@ -147,7 +147,7 @@ class TypeRNet(pl.LightningModule):
         self.val_loss_list.append(loss)
         self.log("val_loss", float(loss))
         if batch_idx % 10 == 0:      
-            grid_in = torchvision.utils.make_grid(convert_rgb_tensor_for_plot(img_in[:4])).permute(1,2,0).cpu().numpy()
+            grid_in = torchvision.utils.make_grid(convert_rgb_tensor_for_plot(img_in[:8])).permute(1,2,0).cpu().numpy()
             grid_out = torchvision.utils.make_grid(torchvision.transforms.functional.invert(convert_gray_tensor_for_plot(out_img[:4])), normalize=True).permute(1,2,0).cpu().numpy()
             mlflow.log_image(grid_in, f'validation_rgb_{self.current_epoch}_{batch_idx}.png')
             mlflow.log_image(grid_out, f'validation_out_{self.current_epoch}_{batch_idx}.png')
@@ -165,7 +165,7 @@ class TypeRNet(pl.LightningModule):
         loss = self.loss.forward(key_strokes, out_img, img_target)
         self.log("test_loss", float(loss))
         if batch_idx % 10 == 0:
-            grid_in = torchvision.utils.make_grid(convert_rgb_tensor_for_plot(img_in[:4])).permute(1,2,0).cpu().numpy()
+            grid_in = torchvision.utils.make_grid(convert_rgb_tensor_for_plot(img_in[:8])).permute(1,2,0).cpu().numpy()
             grid_out = torchvision.utils.make_grid(torchvision.transforms.functional.invert(convert_gray_tensor_for_plot(out_img[:4])), normalize=True).permute(1,2,0).cpu().numpy()
             mlflow.log_image(grid_in, f'test_rgb_{self.current_epoch}_{batch_idx}.png')
             mlflow.log_image(grid_out, f'test_out_{self.current_epoch}_{batch_idx}.png')
